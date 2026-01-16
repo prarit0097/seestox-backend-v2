59cbaee44caffddfc92db0a587c3ed358fc8bcc1
prarit0097 <1995praritsidana@gmail.com>
2026-01-16 16:12:35 +0530

    setting.py update

--- backend/settings.py
+++ backend/settings.py
@@ -132,8 +132,12 @@ TEMPLATES = [
 # --------------------------------------------------
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.postgresql"),
+        "NAME": os.getenv("DB_NAME", "seestox"),
+        "USER": os.getenv("DB_USER", "seestox"),
+        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
